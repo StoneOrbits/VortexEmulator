ddbd8081a73d103af901e6246aec66c36a3a3cdd
Dan <UnrealDanMail@gmail.com>
2023-05-13 21:59:29 -0700

    fixed test maker

--- VortexTestingFramework/tests/make_pattern_tests.sh
+++ VortexTestingFramework/tests/make_pattern_tests.sh
@@ -7,36 +7,37 @@ max_args=5
 max_value=3
 create_test_script_path="./create_test.sh -n"  # Update the path to your create_test.sh script if needed
 
-# Function to generate all combinations of arguments
-generate_args() {
-    local current_arg=$1
-    local current_str=$2
-
-    if [[ $current_arg -le $max_args ]]; then
-        for ((i=0; i<=$max_value; i++)); do
-            generate_args $((current_arg+1)) "${current_str}${i},"
-        done
-    else
-        echo "${current_str%,}"
-    fi
-}
+# 1 core
+# 2 gloves
+# 3 orbit
+# 4 handle
+# 5 finger
+TARGETREPO=5
 
 # Generate tests
 for pattern in ${patterns[@]}; do
-    for colorset in ${colorsets[@]}; do
-        for arg_str in $(generate_args 1 ""); do
-            # Create the command string
-            command_string="-P${pattern} -C${colorset} -A${arg_str}"
+  for colorset in ${colorsets[@]}; do
+    for arg1 in $(seq 0 $max_value); do
+      for arg2 in $(seq 0 $max_value); do
+        for arg3 in $(seq 0 $max_value); do
+          for arg4 in $(seq 0 $max_value); do
+            for arg5 in $(seq 0 $max_value); do
+              # Create the command string
+              command_string="-P${pattern} -C${colorset} -A${arg1},${arg2},${arg3},${arg4},${arg5}"
 
-            # Create the test name
-            test_name="Pattern_${pattern}_Colorset_${colorset//,/}_Args_${arg_str//,/}"
+              # Create the test name
+              test_name="Pattern_${pattern}_Colorset_${colorset//,/}_Args_${arg1}_${arg2}_${arg3}_${arg4}_${arg5}"
 
-            # Create the test description
-            test_descr="Test for pattern ${pattern}, colorset ${colorset} and arguments ${arg_str}"
+              # Create the test description
+              test_descr="Test for pattern ${pattern}, colorset ${colorset} and arguments ${arg1}, ${arg2}, ${arg3}, ${arg4}, ${arg5}"
 
-            # Create the test
-            echo -e "1\n${command_string}\nw100q\n${test_name}\n${test_descr}\n" | $create_test_script_path
+              # Create the test
+              echo -e "$TARGETREPO\n${command_string}\nw100q\n${test_name}\n${test_descr}\n" | $create_test_script_path
+            done
+          done
         done
+      done
     done
+  done
 done
 
